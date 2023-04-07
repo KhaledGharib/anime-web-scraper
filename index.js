a1e7e00a0ae7035aab5ebe9a933d56fdc1c80200
Khaled Gharib <frkhwg@gmail.com>
2023-04-08 00:44:09 +0300

    setup search router and display router

--- index.js
+++ index.js
@@ -4,24 +4,64 @@ const express = require("express");
 const cheerio = require("cheerio");
 const app = express();
 
-const numberOfEpisode = "";
+const data = [];
+app.get("/search", (req, res) => {
+  const animeName = req.query.anime;
+  const url = `https://w1.anime4up.tv/?search_param=animes&s=${animeName}`;
 
-const episode = `https://w1.anime4up.tv/episode/one-piece-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-${numberOfEpisode}/`;
+  axios(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const links = [];
 
-axios(episode).then((res) => {
-  const html = res.data;
-  const $ = cheerio.load(html);
-  const links = [];
-
-  $(".btn.btn-default", html).each(function () {
-    const provider = $(this).text();
-    const link = $(this).attr("href");
-    links.push({
-      provider,
-      link,
+      $(".hover.ehover6", html).each(function () {
+        const link = $(this).find("a").attr("href");
+        const img = $(this).find(".img-responsive").attr("src");
+        links.push({
+          link,
+          img,
+        });
+      });
+      // console.log(links);
+      const linkHTML = links.map((link) => {
+        return `<img src="${link.img}"></img>
+         <a href="${link.link}">click me</a>
+         `;
+      });
+      const responseHTML = linkHTML.join("\n");
+      res.send(responseHTML);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("An error occurred");
     });
-    console.log(links);
-  });
+});
+
+app.get("/episode", (req, res) => {
+  const episode = req.query.episode || 0;
+  const url = `https://w1.anime4up.tv/episode/one-piece-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-${episode}/`;
+
+  axios(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const links = [];
+
+      $(".btn.btn-default", html).each(function () {
+        const provider = $(this).text();
+        const link = $(this).attr("href");
+        links.push({
+          provider,
+          link,
+        });
+      });
+      res.send(links);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("An error occurred");
+    });
 });
 
 app.listen(PORT, () => {
