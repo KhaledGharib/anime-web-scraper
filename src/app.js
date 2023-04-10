9eb2c0e414fc02f644b250f9604ba2ce3a1b6cd5
Khaled Gharib <frkhwg@gmail.com>
2023-04-10 23:36:32 +0300

    ex

--- src/app.js
+++ src/app.js
@@ -19,73 +19,74 @@ hbs.registerPartials(partialsPath);
 let animeName = "";
 
 app.get("", (req, res) => {
-  const animeName = req.query.anime;
-  const url = `https://w1.anime4up.tv/?search_param=animes&s=${animeName}`;
+  //   const animeName = req.query.anime;
+  //   const url = `https://w1.anime4up.tv/?search_param=animes&s=${animeName}`;
 
-  axios(url)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const links = [];
+  //   axios(url)
+  //     .then((response) => {
+  //       const html = response.data;
+  //       const $ = cheerio.load(html);
+  //       const links = [];
 
-      $(".hover.ehover6", html).each(function () {
-        const link = $(this).find("a").attr("href");
-        const img = $(this).find(".img-responsive").attr("src");
-        const title = $(this).find(".img-responsive").attr("alt");
-        links.push({
-          title,
-          link,
-          img,
-        });
-      });
-      // res.send(links);
-      const linkHTML = links.map((link) => {
-        return {
-          title: link.title.replace(/[:! ]/g, "-"),
-          imgSrc: link.img,
-          linkHref: link.link,
-        };
-      });
+  //       $(".hover.ehover6", html).each(function () {
+  //         const link = $(this).find("a").attr("href");
+  //         const img = $(this).find(".img-responsive").attr("src");
+  //         const title = $(this).find(".img-responsive").attr("alt");
+  //         links.push({
+  //           title,
+  //           link,
+  //           img,
+  //         });
+  //       });
+  //       // res.send(links);
+  //       const linkHTML = links.map((link) => {
+  //         return {
+  //           title: link.title.replace(/[:! ]/g, "-"),
+  //           imgSrc: link.img,
+  //           linkHref: link.link,
+  //         };
+  //       });
 
-      res.render("index", { links: linkHTML, animeName });
-    })
-    .catch((error) => {
-      res.status(500).send(error, "An error occurred");
-    });
+  res.render("index");
+  //       res.render("index", { links: linkHTML, animeName });
+  //     })
+  //     .catch((error) => {
+  //       res.status(500).send(error, "An error occurred");
+  //     });
 });
 
-app.get("/episode", (req, res) => {
-  const episode = req.query.episode || 1;
+// app.get("/episode", (req, res) => {
+//   const episode = req.query.episode || 1;
 
-  const url = `https://w1.anime4up.tv/episode/${req.query.anime}-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-${episode}/`;
+//   const url = `https://w1.anime4up.tv/episode/${req.query.anime}-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-${episode}/`;
 
-  axios(url)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const links = [];
+//   axios(url)
+//     .then((response) => {
+//       const html = response.data;
+//       const $ = cheerio.load(html);
+//       const links = [];
 
-      $(".btn.btn-default", html).each(function () {
-        const provider = $(this).text();
-        const link = $(this).attr("href");
-        links.push({
-          provider,
-          link,
-        });
-      });
-      const linkHTML = links.map((link) => {
-        return {
-          provider: link.provider,
-          linkHref: link.link,
-        };
-      });
-      res.render("episode", { links: linkHTML });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).send("An error occurred");
-    });
-});
+//       $(".btn.btn-default", html).each(function () {
+//         const provider = $(this).text();
+//         const link = $(this).attr("href");
+//         links.push({
+//           provider,
+//           link,
+//         });
+//       });
+//       const linkHTML = links.map((link) => {
+//         return {
+//           provider: link.provider,
+//           linkHref: link.link,
+//         };
+//       });
+//       res.render("episode", { links: linkHTML });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       res.status(400).send("An error occurred");
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
